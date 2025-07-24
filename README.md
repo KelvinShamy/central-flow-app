123609344b252dd4db7608d0e6e78d5581b4429a
“KelvinShamy” <kelvinshamy@gmail.com>
2025-07-24 13:02:18 -0700

    establish baseline app architecture

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AstreaxAngularApp
+# CentralFlowApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 15.2.11.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'astreax-angular-app'`, () => {
+  it(`should have as title 'central-flow-app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('astreax-angular-app');
+    expect(app.title).toEqual('central-flow-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('astreax-angular-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('central-flow-app app is running!');
   });
 });
